21497fcaf3ed7e77abd94830ffbae5231d576ad4
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-05-28 11:15:08 -0400

    Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name:"Ashley Westcott", birthday: "01/05/1990", telephone_number: "402-850-7251", img_url: "https://media.licdn.com/dms/image/C4E03AQE_RQMBmDWgaw/profile-displayphoto-shrink_200_200/0?e=1564617600&v=beta&t=4E_XNPLBP1LOtCDi6luiuvshv99qlg4iV5qk20sTtTQ")
+User.create(name:"Adam Robinson", birthday: "13/09/1978", telephone_number: "415-385-0230", img_url: "https://media.bizj.us/view/archive/pacific/user_media/AdamK.-Robinson-218281*220.jpg")
+
+Friendship.create(follower_id: 1, followee_id: 2)
+Friendship.create(follower_id: 2, followee_id: 1)
