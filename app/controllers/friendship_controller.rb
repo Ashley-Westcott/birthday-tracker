f5e3584601d687bcba68dce6f35224a1ccab5326
Adam Robinson <luohaoxiang@yahoo.com>
2019-05-29 17:37:08 -0400

    function of adding friend

--- app/controllers/friendship_controller.rb
+++
@@ -1,9 +0,0 @@
-class FriendshipController < ApplicationController
-
-  def add_friend
-    @user = User.find(params[:id])
-    @user.follower <<
-    Friendship.create(follower_id: @user.id, followee_id: current_user.id)
-  end
-
-end
